bf51a9f2684ee7f10a142a2f7858f747835ba045
Clément Pinard <clement.pinard@xxii.fr>
2023-11-19 23:01:20 +0100

    personal website 1st commit

---
+++ component---src-templates-not-found-template-js-a025fc9c6a57d604bbf6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_lumen=self.webpackChunkgatsby_starter_lumen||[]).push([[41],{8895:function(e,t,l){l.r(t);var n=l(7294),r=l(2922),s=l(9694),a=l(1240),u=l(6089);t.default=()=>{const{title:e,subtitle:t}=(0,u.Z)();return n.createElement(s.Z,{title:"Not Found - "+e,description:t},n.createElement(r.Z,null),n.createElement(a.Z,{title:"NOT FOUND"},n.createElement("p",null,"404 Error - This page does not exist")))}}}]);
+//# sourceMappingURL=component---src-templates-not-found-template-js-a025fc9c6a57d604bbf6.js.map
